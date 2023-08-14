42588ff179af37b66e50ddb2fbabe9f1fbfcf0f8
NadindraDwiA <nadindra.dwi.2005357@students.um.ac.id>
2023-08-14 12:41:28 +0700

    RegisterScreen

--- app/src/main/java/com/septalfauzan/algotrack/ui/RegisterScreen.kt
+++ app/src/main/java/com/septalfauzan/algotrack/ui/RegisterScreen.kt
@@ -1,13 +1,26 @@
 package com.septalfauzan.algotrack.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -24,7 +37,7 @@ import com.septalfauzan.algotrack.ui.component.RoundedTextInput
 import com.septalfauzan.algotrack.ui.theme.AlgoTrackTheme
 
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
+fun RegisterScreen(modifier: Modifier = Modifier){
     Column(
         modifier
             .fillMaxSize()
@@ -34,20 +47,22 @@ fun LoginScreen(modifier: Modifier = Modifier) {
     ) {
         Header(painterResource(id = R.drawable.login_illustration))
         Text(
-            text = stringResource(R.string.login_form_title),
+            text = stringResource(R.string.register_form_title),
             style = MaterialTheme.typography.h5.copy(
                 color = MaterialTheme.colors.primary,
                 fontWeight = FontWeight.Bold
             )
         )
         Spacer(modifier = Modifier.size(8.dp))
-        LoginForm()
+        RegisterForm()
     }
 }
 
 @Composable
-private fun LoginForm(){
+private fun RegisterForm(){
 
+    var name by remember { mutableStateOf("") }
+    var phone by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
@@ -55,6 +70,19 @@ private fun LoginForm(){
         modifier = Modifier.padding(horizontal = 76.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
+        RoundedTextInput(
+            icon = Icons.Default.Person,
+            onChange = { name = it },
+            value = name,
+            modifier = Modifier.fillMaxWidth()
+        )
+        RoundedTextInput(
+            icon = Icons.Default.Phone,
+            onChange = { phone = it },
+            value = phone,
+            keyboardType = KeyboardType.Phone,
+            modifier = Modifier.fillMaxWidth()
+        )
         RoundedTextInput(
             icon = Icons.Default.Email,
             onChange = { email = it },
@@ -71,13 +99,13 @@ private fun LoginForm(){
         )
         Spacer(modifier = Modifier.weight(1f))
         Row {
-            Text(text = stringResource(id = R.string.dont_have_account))
+            Text(text = stringResource(id = R.string.already_have_account))
             Text(
-                text = stringResource(id = R.string.register),
+                text = stringResource(id = R.string.login),
                 color = MaterialTheme.colors.primary,
                 modifier = Modifier.clickable { /* go to register screen */ })
         }
-        LogRegButton(text = stringResource(id = R.string.login), onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth())
+        LogRegButton(text = stringResource(R.string.register), onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth())
     }
 }
 
@@ -85,6 +113,6 @@ private fun LoginForm(){
 @Composable
 private fun Preview() {
     AlgoTrackTheme {
-        LoginScreen()
+        RegisterScreen()
     }
 }
--- app/src/main/java/com/septalfauzan/algotrack/ui/component/LogRegEdtText.kt
+++ app/src/main/java/com/septalfauzan/algotrack/ui/component/LogRegEdtText.kt
@@ -1,11 +1,19 @@
 package com.septalfauzan.algotrack.ui.component
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,28 +36,38 @@ sealed class ErrorCondition {
  */
 @Composable
 fun RoundedTextInput(
-    label: String,
+    icon: ImageVector,
     onChange: (String) -> Unit,
     value: String,
     keyboardType: KeyboardType = KeyboardType.Text,
     modifier: Modifier = Modifier
 ) {
     Column {
-        Text(text = label, style = MaterialTheme.typography.h6.copy(color = Gray))
-        OutlinedTextField(
-            value = value,
-            onValueChange = onChange,
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                unfocusedBorderColor = MaterialTheme.colors.secondary,
-                focusedBorderColor = MaterialTheme.colors.primary,
-                errorBorderColor = RedAccent
-            ),
-            modifier = modifier,
-            shape = RoundedCornerShape(16.dp),
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = keyboardType
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = Gray,
+                modifier = Modifier.padding(start = 8.dp)
             )
-        )
+            OutlinedTextField(
+                value = value,
+                onValueChange = onChange,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    unfocusedBorderColor = MaterialTheme.colors.secondary,
+                    focusedBorderColor = MaterialTheme.colors.primary,
+                    errorBorderColor = RedAccent
+                ),
+                modifier = modifier.height(50.dp),
+                shape = RoundedCornerShape(16.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = keyboardType
+                )
+            )
+        }
     }
 }
 
@@ -58,9 +76,9 @@ fun RoundedTextInput(
 private fun Preview() {
     var value by remember { mutableStateOf("") }
 
-    AlgoTrackTheme() {
-        Surface() {
-            RoundedTextInput(label = "Lorem", onChange = { value = it }, value = value)
+    AlgoTrackTheme {
+        Surface {
+            RoundedTextInput(icon = Icons.Default.Email, onChange = { value = it }, value = value)
         }
     }
 }
